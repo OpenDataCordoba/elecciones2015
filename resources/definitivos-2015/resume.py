aad04580efb474fcfd601297f0b50e33b89eb7f9
avdata99 <andres@data99.com.ar>
2015-10-24 19:58:20 -0300

    add porc

--- resources/definitivos-2015/resume.py
+++ resources/definitivos-2015/resume.py
@@ -64,9 +64,12 @@ for p in provincias:
         res[p]['votos_anulados'] = clean(rows[32][2])
         res[p]['votos_total'] = clean(rows[33][2])
 
-        res[p]['131'] = {'3166': clean(rows[4][2]), 'total': clean(rows[4][2])}
+        res[p]['131'] = {'total': clean(rows[4][2])}
+        res[p]['131']['porc'] = clean_porc(rows[4][3])
+        res[p]['131']['3166'] = clean(rows[4][2])
         
         res[p]['135'] = {'total': clean(rows[6][2])}
+        res[p]['135']['porc'] = clean_porc(rows[6][3])
         res[p]['135']['3182'] = clean(rows[7][2])
         res[p]['135']['3185'] = clean(rows[8][2])
         res[p]['135']['3243'] = clean(rows[9][2])
@@ -77,18 +80,34 @@ for p in provincias:
             sys.exit()
         
         res[p]['138'] = {'total': clean(rows[10][2])}
+        res[p]['138']['porc'] = clean_porc(rows[10][3])
         res[p]['138']['3192'] = clean(rows[11][2])
         res[p]['138']['3214'] = clean(rows[12][2])
-        res[p]['132'] = {'3008': clean(rows[13][2]), 'total': clean(rows[13][2])}        
+        res[p]['132'] = {'3008': clean(rows[13][2]), 'total': clean(rows[13][2])}
+        res[p]['132']['porc'] = clean_porc(rows[13][3])
+        
         res[p]['137'] = {'total': clean(rows[15][2])}
+        res[p]['137']['porc'] = clean_porc(rows[15][3])
         res[p]['137']['3014'] = clean(rows[16][2])
         res[p]['137']['3261'] = clean(rows[17][2])
         res[p]['133'] = {'3100': clean(rows[18][2]), 'total': clean(rows[18][2])}
+        res[p]['133']['porc'] = clean_porc(rows[18][3])
+        
         res[p]['136'] = {'3234': clean(rows[20][2]), 'total': clean(rows[20][2])}        
+        res[p]['136']['porc'] = clean_porc(rows[20][3])
+        
         res[p]['13'] = {'3253': clean(rows[22][2]), 'total': clean(rows[22][2])}
+        res[p]['13']['porc'] = clean_porc(rows[22][3])
+        
         res[p]['134'] = {'3018': clean(rows[24][2]), 'total': clean(rows[24][2])}
+        res[p]['134']['porc'] = clean_porc(rows[24][3])
+        
         res[p]['81'] = {'3217': clean(rows[26][2]), 'total': clean(rows[26][2])}
+        res[p]['81']['porc'] = clean_porc(rows[26][3])
+        
         res[p]['57'] = {'3172': clean(rows[28][2]), 'total': clean(rows[28][2])}
+        res[p]['57']['porc'] = clean_porc(rows[28][3])
+        
 
         # check general
         votos_positivos = 0
@@ -97,6 +116,19 @@ for p in provincias:
         if votos_positivos != res[p]['votos_positivos']:
             print "BAD total"
             sys.exit()
+
+        porc = 0
+        for a in alianzas:
+            porc += res[p][str(a)]['porc']
+            
+        if porc < 99 or porc > 101:
+            print "BAD porc"
+            sys.exit()
+
+        if votos_positivos != res[p]['votos_positivos']:
+            print "BAD total"
+            sys.exit()
+
 
         if p != 99:
             votos_positivos_total += votos_positivos
@@ -124,7 +156,7 @@ if votos_positivos_total != res[99]['votos_positivos']:
 sinlema = []
 for p in provincias:
     for a in alianzas:
-        sinlema.append({"provincia":p,"alianza":a,"votos":res[p][str(a)]['total']})
+        sinlema.append({"provincia":p,"alianza":a,"votos":res[p][str(a)]['total'],"porc":res[p][str(a)]['porc']})
 
 print "**********\nSin Lemas\n**********\n"
 print str(sinlema)
