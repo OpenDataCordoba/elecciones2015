27bcc199d90ae03f415ae4380e2a48e761684f89
avdata99 <andres@data99.com.ar>
2015-10-26 01:39:19 -0300

    add update

--- api/csv/bajada.py
+++ api/csv/bajada.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+import os
 
 from requests.auth import HTTPBasicAuth
 
-
 resp = requests.get('http://descargas.resultados.gob.ar/desca_nac/descargas.htm', auth=HTTPBasicAuth('DES0062', 'WYYM7329'))
-link = "http://descargas.resultados.gob.ar/desca_nac/" + re.findall('a href\=\"(DATOS_\d+.tar.gz)', resp.text)[0]
-print link
+jfile = re.findall('a href\=\"(DATOS_\d+.tar.gz)', resp.text)[0]
+link = "http://DES0062:WYYM7329@descargas.resultados.gob.ar/desca_nac/" + jfile
+print jfile
+
+os.system('wget ' + link)
+os.system('tar xvzf ' + jfile)
+os.system('mv ' + jfile + '/* .')
+
+os.system('wget http://opendatacordoba.org/elecciones2015/api/?do=all')
