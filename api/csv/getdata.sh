75b34cfaf9bc59aeb283e5d59be9ea27392e7396
avdata99 <andres@data99.com.ar>
2015-10-25 23:40:32 -0300

    getData

---
+++ api/csv/getdata.sh
@@ -0,0 +1,8 @@
+wget http://descargas.resultados.gob.ar/desca_nac/generales_000.tar.gz
+tar xvzf generales_000.tar.gz 
+
+wget http://descargas.resultados.gob.ar/desca_nac/DATOS_20193448.tar.gz
+tar xvzf DATOS_20193448.tar.gz
+
+mv DATOS_20193448/* .
+mv generales_000/* .
